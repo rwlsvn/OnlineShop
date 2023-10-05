ccbafc82bc97b58d7c10eef590af142bc05812b4
rwlsvn <pashaleshkevich@gmail.com>
2023-10-05 18:44:18 +0300

    add appuser model and users db context into the library

---
+++ server/OnlineShop.Library/Models/AppUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace OnlineShop.Library.Models
+{
+    public class AppUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string? RefreshToken { get; set; }
+        public DateTime RefreshTokenExpiryTime { get; set; }
+    }
+}
