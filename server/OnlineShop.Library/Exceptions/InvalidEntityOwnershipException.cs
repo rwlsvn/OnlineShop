9497eb62b7fedc1c1781cfa1b16bd805ddd97766
rwlsvn <pashaleshkevich@gmail.com>
2023-10-14 23:33:49 +0300

    add invalid ownership exception

---
+++ server/OnlineShop.Library/Exceptions/InvalidEntityOwnershipException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Library.Exceptions
+{
+    public class InvalidEntityOwnershipException : Exception
+    {
+        public InvalidEntityOwnershipException
+            (string entityName, object entityKey, Guid userId)
+            : base($"User '{userId}' does not have ownership or " +
+                  $"permission to access the '{entityName}' ({entityKey})")
+        { }
+    }
+}
