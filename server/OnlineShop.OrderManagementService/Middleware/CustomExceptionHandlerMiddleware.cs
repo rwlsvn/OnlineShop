e6ed02dd5078845e97b6d2aeb4e7b183295538a3
rwlsvn <pashaleshkevich@gmail.com>
2023-10-14 23:43:02 +0300

    add exception handler middleware

--- server/OnlineShop.OrderManagementService/Middleware/CustomExceptionHandlerMiddleware.cs
+++ server/OnlineShop.OrderManagementService/Middleware/CustomExceptionHandlerMiddleware.cs
@@ -1,6 +1,52 @@
-﻿namespace OnlineShop.OrderManagementService.Middleware
+﻿using OnlineShop.Library.Exceptions;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Text.Json;
+
+namespace OnlineShop.OrderManagementService.Middleware
 {
     public class CustomExceptionHandlerMiddleware
     {
+        readonly RequestDelegate _next;
+
+        public CustomExceptionHandlerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception exception)
+            {
+                await HandleExceptionAsync(context, exception);
+            }
+        }
+
+        private Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            var code = HttpStatusCode.InternalServerError;
+            var jsonResponse = JsonSerializer.Serialize(new { message = exception.Message });
+
+            switch (exception)
+            {
+                case ValidationException:
+                    code = HttpStatusCode.BadRequest;
+                    break;
+                case EntityNotFoundException:
+                    code = HttpStatusCode.NotFound;
+                    break;
+                case InvalidEntityOwnershipException:
+                    code = HttpStatusCode.BadRequest;
+                    break;
+            }
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)code;
+
+            return context.Response.WriteAsync(jsonResponse);
+        }
     }
 }
